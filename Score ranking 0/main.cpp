3b8a79794925a15a7acbad989156c944bbeec95a
bowen163 <763893371@qq.com>
2019-08-27 17:40:54 +0800

    Initial Commit

---
+++ Score ranking 0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Score ranking 0
+//
+//  Created by s20181102936 on 2019/8/27.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
